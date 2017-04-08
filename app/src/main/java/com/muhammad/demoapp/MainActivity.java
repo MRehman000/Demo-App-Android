e06801dabe0dbc50bdd586f25bdec79816b1a58c
mrehman0 <muh316@gmail.com>
2017-04-07 23:45:05 -0400

    Working with new fields

--- app/src/main/java/com/muhammad/demoapp/MainActivity.java
+++ app/src/main/java/com/muhammad/demoapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.muhammad.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void ClickFunction(View view){
+
+        EditText NameField = (EditText) findViewById(R.id.NameInput);
+        Log.i("INFO", "You clicked the button!" + NameField.getText().toString());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
